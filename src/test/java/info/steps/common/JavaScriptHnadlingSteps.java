41bfdd48f197288988c6ab400707bc209e4f3bdf
hamzaqch <hamza.qch@gmail.com>
2023-04-16 03:43:18 -0400

    JavaScriptHandling preDefinedSteps

---
+++ src/test/java/info/steps/common/JavaScriptHnadlingSteps.java
@@ -0,0 +1,23 @@
+package info.steps.common;
+
+import info.pom.AbstractPage;
+import io.cucumber.java.en.Then;
+
+public class JavaScriptHnadlingSteps extends AbstractPage {
+
+	/**
+	 * Step to handle java script
+	 */
+    @Then("^I accept alert$")
+    public void handle_alert() {
+    	javaScriptHandling_Obj.handleAlert("accept");
+    }
+	
+ /**
+  * Steps to dismiss java script
+  */
+    @Then("^I dismiss alert$")
+    public void dismiss_alert() {
+    	javaScriptHandling_Obj.handleAlert("dismiss");
+    }
+}
